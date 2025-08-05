320e69bd75ad6de60b7f44d64410b5df04d0bdb8
zaxariasSk <zack7abc7@gmail.com>
2025-08-06 00:05:05 +0300

    builded website until services

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,10 @@
-module.exports = () => ({});
+module.exports = () => ({
+    seo: {
+        enabled: true,
+    },
+    'strapi-v5-plugin-populate-deep': {
+        config: {
+            defaultDepth: 3, // Default is 5
+        }
+    },
+});
---
+++ types/generated/components.d.ts
@@ -0,0 +1,134 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface HomePageWhoWeAre extends Struct.ComponentSchema {
+  collectionName: 'components_home_page_who_we_ares';
+  info: {
+    displayName: 'who we are';
+  };
+  attributes: {
+    description: Schema.Attribute.Blocks;
+    link_label: Schema.Attribute.String;
+    link_url: Schema.Attribute.String;
+    profile_picture: Schema.Attribute.Media<
+      'images' | 'files' | 'videos' | 'audios'
+    >;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface HomePageWhyWorkWithUs extends Struct.ComponentSchema {
+  collectionName: 'components_home_page_why_work_with_uses';
+  info: {
+    displayName: 'Why Work With Us';
+  };
+  attributes: {
+    description: Schema.Attribute.RichText & Schema.Attribute.Required;
+    icon: Schema.Attribute.Media<'images' | 'files' | 'videos' | 'audios'> &
+      Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface MenuCta extends Struct.ComponentSchema {
+  collectionName: 'components_menu_ctas';
+  info: {
+    displayName: 'CTA';
+  };
+  attributes: {
+    text: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface MenuNavigationLinks extends Struct.ComponentSchema {
+  collectionName: 'components_menu_navigation_links';
+  info: {
+    displayName: 'Navigation links';
+    icon: 'bulletList';
+  };
+  attributes: {
+    label: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+export interface SharedMedia extends Struct.ComponentSchema {
+  collectionName: 'components_shared_media';
+  info: {
+    displayName: 'Media';
+    icon: 'file-video';
+  };
+  attributes: {
+    file: Schema.Attribute.Media<'images' | 'files' | 'videos'>;
+  };
+}
+
+export interface SharedQuote extends Struct.ComponentSchema {
+  collectionName: 'components_shared_quotes';
+  info: {
+    displayName: 'Quote';
+    icon: 'indent';
+  };
+  attributes: {
+    body: Schema.Attribute.Text;
+    title: Schema.Attribute.String;
+  };
+}
+
+export interface SharedRichText extends Struct.ComponentSchema {
+  collectionName: 'components_shared_rich_texts';
+  info: {
+    description: '';
+    displayName: 'Rich text';
+    icon: 'align-justify';
+  };
+  attributes: {
+    body: Schema.Attribute.RichText;
+  };
+}
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    description: '';
+    displayName: 'Seo';
+    icon: 'allergies';
+    name: 'Seo';
+  };
+  attributes: {
+    metaDescription: Schema.Attribute.Text & Schema.Attribute.Required;
+    metaTitle: Schema.Attribute.String & Schema.Attribute.Required;
+    shareImage: Schema.Attribute.Media<'images'>;
+  };
+}
+
+export interface SharedSlider extends Struct.ComponentSchema {
+  collectionName: 'components_shared_sliders';
+  info: {
+    description: '';
+    displayName: 'Slider';
+    icon: 'address-book';
+  };
+  attributes: {
+    caption: Schema.Attribute.String;
+    files: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    title: Schema.Attribute.String;
+    url: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'home-page.who-we-are': HomePageWhoWeAre;
+      'home-page.why-work-with-us': HomePageWhyWorkWithUs;
+      'menu.cta': MenuCta;
+      'menu.navigation-links': MenuNavigationLinks;
+      'shared.media': SharedMedia;
+      'shared.quote': SharedQuote;
+      'shared.rich-text': SharedRichText;
+      'shared.seo': SharedSeo;
+      'shared.slider': SharedSlider;
+    }
+  }
+}
